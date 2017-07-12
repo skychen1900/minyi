d9bc9df54019cccbbfd097180f37f6fc60ced76c
BI944015 <BI944015@IBM099-R90LKS00>
2017-07-12 22:26:58 +0900

    初版

---
+++ minyix/src/main/java/com/skychen/minyi/MinyixApplication.java
@@ -0,0 +1,12 @@
+package com.skychen.minyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinyixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinyixApplication.class, args);
+	}
+}
